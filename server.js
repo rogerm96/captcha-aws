2d07ae0feab564bd827cfd3dd3e27df589fb9f95
Roger <rogermanzo04@gmail.com>
2021-04-07 18:34:35 -0400

    update port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(morgan('dev'));
 
 app.get('/', (req,res) => res.send('<h1>Te amo Ane</h1>'));
 
-app.listen(8080, () => {
-    console.log('run server on port 8080!');
+app.listen(3000, () => {
+    console.log('run server on port 3000!');
 });
