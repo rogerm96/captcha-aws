b53523af4a174851efbcd3e70a54d4fc70f53a9d
Roger <rogermanzo04@gmail.com>
2021-04-07 18:26:34 -0400

    modified file static

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
-});
+app.get('/', (req,res) => res.send('<h1>Te amo Ane</h1>'));
 
 app.listen(3000, () => {
     console.log('run server on port 3000!');
