a44622e3861da1189b5a79151f875bbbcb69fb2c
Roger <rogermanzo04@gmail.com>
2021-04-15 08:55:45 -0400

    config options for selenium

--- getCaptcha.py
+++ getCaptcha.py
@@ -3,13 +3,19 @@ import PIL
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
 x = 1
 
 while x <= 1:
 
-    browser = webdriver.Chrome(executable_path=r"./driver/chromedriver")
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    browser = webdriver.Chrome(executable_path=r"./driver/chromedriver", chrome_options=chrome_options)
 
     browser.get('http://contribuyente.seniat.gob.ve/iseniatlogin/contribuyente.do')
 
